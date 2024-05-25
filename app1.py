c7f586f663b3aa3d27051a93129eead02ed5005f
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-05-25 17:02:24 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -190,7 +190,6 @@ def train_regression_model(features, target):
 
 
 def train_classification_model(features, target):
-    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
     # Train the logistic regression model
